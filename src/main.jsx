aeefb59e68ea5cb6c8b93355404214ba8e5dc334
ronak-bansal <ronakbansal022@gmail.com>
2025-08-07 15:32:06 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import CoinContextProvider from './context/CoinContext.jsx'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <CoinContextProvider>
+        <App />
+    
+       </CoinContextProvider>
+    </BrowserRouter>
+    
+  </StrictMode>
+)
---
+++ src/pages/Coin/Coin.jsx
@@ -0,0 +1,87 @@
+import React , {useContext, useEffect, useState} from 'react'
+import './Coin.css'
+import { useParams } from 'react-router-dom'
+import { CoinContext } from '../../context/CoinContext'
+import LineChart from '../../components/LineChart/LineChart'
+
+const coin = () => {
+
+  const {coinId} = useParams();
+
+  const [coinData, setCoinData] = useState();
+  const [historicalData, setHistoricalData] = useState();
+
+  const {currency} = useContext(CoinContext)
+  
+  const fetchCoinData = async ()=>{
+    const options = {
+      method: 'GET',
+      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-rLDMe3ixhuEuQfewkxruhfWQ'}
+    };
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then(res => res.json())
+      .then(res => setCoinData(res))
+      .catch(err => console.error(err));
+  }
+  const fetchHistoricalData = async()=>{
+    const options = {
+      method: 'GET',
+      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-rLDMe3ixhuEuQfewkxruhfWQ'}
+    };
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`, options)
+      .then(res => res.json())
+      .then(res => setHistoricalData(res))
+      .catch(err => console.error(err));
+  }
+
+
+
+
+  useEffect(()=>{
+    fetchCoinData(); 
+    fetchHistoricalData();
+  },[currency])
+
+
+
+if(coinData && historicalData){
+  return (
+    <div className='coin'>
+      <div className="coin-name">
+        <img src={coinData.image.large} alt="" />
+        <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+      </div>
+      <div className="coin-chart">
+        <LineChart historicalData={historicalData}/>
+      </div>
+      <div className="coin-info">
+        <ul>
+          <li>Crypto Market Rank</li>
+          <li>{coinData.market_cap_rank}</li>
+        </ul>
+        <ul>
+          <li>Crypto Price</li>
+          <li>{currency.symbol} {coinData.market_data.current_price[currency.name].toLocaleString()}</li>
+        </ul>
+        <ul>
+          <li>Market cap</li>
+          <li>{currency.symbol} {coinData.market_data.market_cap[currency.name].toLocaleString()}</li>
+        </ul>
+      </div>
+    </div>
+      )
+}else{
+  return (
+    <div className='spinner'>
+      <div className="spin">
+
+      </div>
+      
+    </div>
+  )
+}
+}
+
+export default coin
